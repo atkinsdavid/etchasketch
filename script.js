3f7f3c9dd69b6a3795b6c72edc762dc2c6c6ab18
David Atkins <david.atkins@me.com>
2022-07-26 09:04:35 -0500

    added script for bg color change; centered elements

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById('grid-container');
+const cells = document.getElementsByClassName('cell');
 
 for (let i = 1 ; i <= 16 ; i++) {
     const row = document.createElement('div');
@@ -8,8 +9,16 @@ for (let i = 1 ; i <= 16 ; i++) {
     for (let n = 1 ; n <= 16 ; n++) {
         const cell = document.createElement('div'); 
         row.appendChild(cell);
-        cell.setAttribute('id','cell' + n);
+        cell.setAttribute('id','cell_' + n*i);
         cell.classList.add('cell');
-        cell.textContent = 'Cell ' + n;
+        // cell.textContent = cell.id;
     };
+};
+
+for (let i = 0; i < cells.length ; i++) {
+    // function func() { console.log(cells[i].id);};
+    // cells[i].onclick = console.log(cells[i].id);
+    cells[i].addEventListener('mouseover', () => {
+        cells[i].style.backgroundColor = "black";
+      });
 };
