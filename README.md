570495248226f44be1c1e0979d3a6518884db5f8
David Atkins <david.atkins@me.com>
2022-07-26 16:08:26 -0500

    added new and clear buttons

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 - Create a webpage with a 16x16 grid of square divs.
     - Create the divs using JavaScript.
     - Use CSS to make divs appear as grid instead of separate lines
-- 
+- Make each cell turn black when cursor passes through
+    - randomize colors instead of black
+    - increase black level on each pass so that >10 passes give completely black cell
+- Add reset button to clear all cells
+- Add prompt to create new grid with a given size up to 100 
--- script.js
+++ script.js
@@ -1,32 +1,62 @@
+const newBtn = document.getElementById('new-grid');
+const clearBtn = document.getElementById('reset');
 const container = document.getElementById('grid-container');
-const cells = document.getElementsByClassName('cell');
+let cells = [];
 
-for (let i = 1 ; i <= 16 ; i++) {
-    const row = document.createElement('div');
-    container.appendChild(row);
-    row.setAttribute('id','row' + i);
-    row.classList.add('row');
-    for (let n = 1 ; n <= 16 ; n++) {
-        const cell = document.createElement('div'); 
-        row.appendChild(cell);
-        cell.setAttribute('id','cell_' + n*i);
-        cell.classList.add('cell');
-        // cell.textContent = cell.id;
+function newGrid(){
+    let num = prompt("Please enter a whole number between 1-100.")
+    if (isNaN(num)){
+        alert("Input must be a number.")
+    } else if (num > 100 || num < 1) {
+        alert("Input must be between 1-100.")
+    } else if (Number.isInteger(num*1)) {
+        while (container.firstChild){
+            container.removeChild(container.firstChild);
+        };
+        for (let i = 1 ; i <= num ; i++) {
+            const row = document.createElement('div');
+            container.appendChild(row);
+            row.setAttribute('id','row_' + i);
+            row.classList.add('row');
+            for (let n = 1 ; n <= num ; n++) {
+                const cell = document.createElement('div'); 
+                row.appendChild(cell);
+                cell.setAttribute('id','cell_' + n*i);
+                cell.setAttribute('light',50);
+                cell.classList.add('cell');
+            }
+        }
+        cells = document.getElementsByClassName('cell');
+    } else {
+        alert("Input must be a whole number (no decimals or fractions).")
+    };
+    for (let i = 0; i < cells.length ; i++) {
+        cells[i].addEventListener('mouseover', () => {
+            let cellLightness = Number(cells[i].getAttribute('light'));
+            function randomInteger(max) {
+                return Math.floor(Math.random()*(max + 1));
+            };
+            function randomColor() {
+                let hue = randomInteger(360);
+                let sat = randomInteger(100);
+                return `hsl(${hue},${sat}%,${cellLightness}%)`;
+            };
+            cells[i].style.backgroundColor = randomColor();
+            if (cellLightness >0) {
+                cellLightness -= 5;
+            }
+            cells[i].setAttribute('light',cellLightness);
+        });
     };
 };
 
-for (let i = 0; i < cells.length ; i++) {
-    let cellLightness = 50;
-    cells[i].addEventListener('mouseover', () => {
-        function randomInteger(max) {
-            return Math.floor(Math.random()*(max + 1));
-        };
-        function randomColor() {
-            let hue = randomInteger(360);
-            let sat = randomInteger(100);
-            return `hsl(${hue},${sat}%,${cellLightness}%)`;
-        };
-        cells[i].style.backgroundColor = randomColor();
-        cellLightness -= 5;
-      });
-};
+
+newBtn.addEventListener( 'click' , newGrid);
+
+clearBtn.addEventListener( 'click' , () => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.style.backgroundColor = '';
+        cell.setAttribute('light',50);
+    });
+});
